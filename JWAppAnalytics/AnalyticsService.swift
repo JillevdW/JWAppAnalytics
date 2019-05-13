33397256dc0ebb255c19f9f2594482825eaccaa8
Jille <jille.van.der.weerd@beeproger.com>
2019-05-13 12:38:14 +0200

    adds a way to track the user journey

--- JWAppAnalytics/AnalyticsService.swift
+++ JWAppAnalytics/AnalyticsService.swift
@@ -12,17 +12,28 @@ public class AnalyticsService {
     
     public static let shared = AnalyticsService()
     
+    private var userJourneyEnabled = false
+    
     private var apiUrl: String?
     
-    public func setup(withUrl url: String) {
-        apiUrl = url
+    private var userJourney: UserJourney?
+    
+    public func setup(withUrl url: String, userJourneyEnabled: Bool = false) {
+        apiUrl = "\(url)/app-analytics-api"
         if UserDefaults.standard.string(forKey: "uuid") == nil {
             UserDefaults.standard.set(UIDevice.current.identifierForVendor!.uuidString, forKey: "uuid")
         }
+        self.userJourneyEnabled = userJourneyEnabled
+        if userJourneyEnabled {
+            if let uuid = UserDefaults.standard.string(forKey: "uuid") {
+                userJourney = UserJourney(uuid: uuid, events: [[String: Any]]())
+            }
+        }
     }
     
+    /// Triggers an event for the metric of the given String.
     public func trigger(event: String) {
-        guard let apiUrl = apiUrl, let url = URL(string: "\(apiUrl)/app-analytics-api/events") else { return }
+        guard let apiUrl = apiUrl, let url = URL(string: "\(apiUrl)/events") else { return }
         var request = URLRequest(url: url)
         let uuid = UserDefaults.standard.string(forKey: "uuid")!
         let json = [
@@ -36,12 +47,59 @@ public class AnalyticsService {
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = jsonData
         
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            guard error == nil else {
+                print("Error sending analytics.")
+                return
+            }
+        }.resume()
         
+        addToUserJourney(event: event)
+    }
+    
+    public func willEnterForeground() {
+        userJourney?.events = [[String: Any]]()
+    }
+    
+    public func didEnterBackground() {
+        guard let userJourney = userJourney else { return }
+        send(userJourney: userJourney)
+    }
+    
+    private func addToUserJourney(event: String) {
+        guard userJourneyEnabled, let userJourney = userJourney else { return }
+        let formatter = DateFormatter()
+        formatter.dateFormat = "Y-MM-dd HH:mm:ss"
+        let now = formatter.string(from: Date())
+        userJourney.events.append([
+            "event": event,
+            "date": now
+        ])
+    }
+    
+    private func send(userJourney: UserJourney) {
+        guard let apiUrl = apiUrl, let url = URL(string: "\(apiUrl)/app-session") else { return }
+        var request = URLRequest(url: url)
+        let uuid = UserDefaults.standard.string(forKey: "uuid")!
+
+        let json: [String: Any] = [
+            "device_id": userJourney.uuid,
+            "events": userJourney.events
+        ]
+        
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
         
         URLSession.shared.dataTask(with: request) { (_, response, error) in
             guard error == nil else {
                 print("Error sending analytics.")
                 return
+            }
+            if let res = response as? HTTPURLResponse {
+                // Handle either deleting the json or saving it to disk based on statuscode.
             }
         }.resume()
     }
